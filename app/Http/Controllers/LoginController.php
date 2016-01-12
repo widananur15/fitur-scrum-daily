94197eb57490caefc404095ba7a2483f42a9331f
chrismerrari <chrismerrari78@gmail.com>
2016-01-12 16:19:01 +0700

    login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LoginUser;
+use Illuminate\Support\Facades\Redirect;
+use Session;
+use App\Services\LoginService;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected $loginService;
+
+    public function __construct(LoginService $LoginService){
+        $this->loginService=$LoginService;
+    }
+
+    public function index()
+    {
+        return View('login/login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $login_user = $request->login_user;
+        $password = $request->password;
+
+        $user = $this->loginService->doLogin($login_user,$password);
+
+        if($user){
+            Session::put('loggeduser',$user);
+                return View('scrum/scrum');
+        }else{
+            return Redirect('/login')
+                    ->with('message','invalid username or password');
+        }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
