dccde2b8f43a3d9b4ef60e58e739c33411bf4a74
sts <widana@solusiteknologi.co.id>
2016-01-12 14:23:22 +0700

    new

--- database/migrations/2016_01_12_071657_create_table_t_user.php
+++ database/migrations/2016_01_12_071657_create_table_t_user.php
@@ -18,7 +18,6 @@ class CreateTableTUser extends Migration
             $table->string('email');
             $table->string('username');
             $table->string('password');
-            $table->string('flg_active',2);
             $table->string('active', 1)->default('Y');
             $table->string('active_datetime', 14);
             $table->string('non_active_datetime', 14);
