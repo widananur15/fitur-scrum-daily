48750f6abe3ab83b3c7509abb106191f75037fe1
chrismerrari <chrismerrari78@gmail.com>
2016-01-13 09:35:32 +0700

    commit

---
+++ app/Http/Middleware/LoginMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!$request->session()->has('loggeduser')){
+            return Redirect('/login')
+                ->with('message','Silakan login terlebih dahulu');
+        }
+
+        return $next($request);
+    }
+}
